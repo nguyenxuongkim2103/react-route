98fcd21adfaed4c3b802da550183a394d5f704f9
P2356 <p2356@student.jamk.fi>
2021-05-21 16:14:15 +0300

    Ádding sorting query parameters

--- src/components/pages/AllQuotes.js
+++ src/components/pages/AllQuotes.js
@@ -4,7 +4,7 @@ const DUMMY_QUOTES = [
     {id: 'q1', author: 'Kim', text: 'Test quote no 1'},
     {id: 'q2', author: 'Jon', text: 'Test quote no 2'},
     {id: 'q3', author: 'Mike', text: 'Test quote no 3'},
-    {id: 'q4', author: 'Lesly', text: 'Test quote no 1'},
+    {id: 'q4', author: 'Lesly', text: 'Test quote no 4'},
 ];
 
 
